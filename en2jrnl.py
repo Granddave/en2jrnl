fde33f802ad08de9a7f5922178399cd71bc1af1f
David Isaksson <davidisaksson93@gmail.com>
2020-05-02 10:13:16 +0200

    Format code

--- en2jrnl.py
+++ en2jrnl.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python3
-import html2text
+import argparse
 import os
 import re
-import argparse
 
-def stripEmptyLines(s):
-    lines = s.splitlines()
+import html2text
+
+
+def _strip_empty_lines(multiline_text):
+    lines = multiline_text.splitlines()
     while lines and not lines[0].strip():
         lines.pop(0)
     while lines and not lines[-1].strip():
         lines.pop()
     return '\n'.join(lines)
 
-def dir_path(string):
+
+def _dir_path(string):
     if os.path.isdir(string):
         return string
     else:
         raise NotADirectoryError(string)
+
 
 class Post():
     def __init__(self, filepath):
@@ -25,9 +29,9 @@ class Post():
         self.content = None
         self.title = None
 
-    def parseFile(self):
+    def parse_file(self):
         if self.filepath is "":
-            Print("Filename is empty")
+            print("Filename is empty")
             return False
 
         try:
@@ -35,12 +39,13 @@ class Post():
                 content = f.read()
                 h = html2text.HTML2Text()
                 h.ignore_links = True
-                htmlContent = h.handle(content)
+                html_content = h.handle(content)
 
                 # Parse date.
-                dateRegex = re.findall(r'\*\*Created:\*\*\|.._(\d*-\d*-\d* \d*:\d*)_', htmlContent)
-                if len(dateRegex) > 0:
-                    self.datetime = dateRegex[0]
+                date_regex = re.findall(r'\*\*Created:\*\*\|.._(\d*-\d*-\d* \d*:\d*)_',
+                                       html_content)
+                if len(date_regex) > 0:
+                    self.datetime = date_regex[0]
                     print("Date:  " + self.datetime)
                 else:
                     print("Could not find date")
@@ -48,12 +53,12 @@ class Post():
 
                 # Set the first row as title, but without the pound sign before.
                 # Example: '# My title' -> 'My title'
-                self.title = htmlContent.split('\n', 1)[0][2:]
+                self.title = html_content.split('\n', 1)[0][2:]
                 print("Title: " + self.title)
 
                 # Remove date and empty lines. Any other better way?
-                self.content = '\n'.join(htmlContent.split('\n')[5:])
-                self.content = stripEmptyLines(self.content)
+                self.content = '\n'.join(html_content.split('\n')[5:])
+                self.content = _strip_empty_lines(self.content)
 
                 # Remove wierd trailing null byte.
                 self.content = self.content.rstrip('\0')
@@ -63,45 +68,54 @@ class Post():
             print(e)
             return False
 
-    def writeToFile(self, outputFile):
+    def write_to_file(self, outputFile):
         with open(outputFile, "a") as f:
             f.write(self.datetime + ' ' + self.title + '\n')
             f.write(self.content)
 
-def main():
+
+def _main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", help="directory with exported html files to convert", type=dir_path, nargs=1, required=True)
-    parser.add_argument("-o", "--output", help="jrnl output file", nargs=1)
+    parser.add_argument("-i",
+                        "--input",
+                        help="directory with exported html files to convert",
+                        type=_dir_path,
+                        nargs=1, required=True)
+    parser.add_argument("-o",
+                        "--output",
+                        help="jrnl output file",
+                        nargs=1)
     args = parser.parse_args()
 
-    outputFile = "jrnl.txt"
+    output_file = "jrnl.txt"
     if args.output is not None:
-        outputFile = args.output[0]
+        output_file = args.output[0]
 
-    fileDir = args.input[0]
-    inputFiles = sorted(os.listdir(fileDir))
-    inputFiles = [f for f in inputFiles if f.endswith(".html") and "index" not in f]
+    file_dir = args.input[0]
+    input_files = sorted(os.listdir(file_dir))
+    input_files = [f for f in input_files
+                   if f.endswith(".html") and "index" not in f]
 
-    print("Input directory:", fileDir)
-    print("Output file:    ", outputFile)
+    print("Input directory:", file_dir)
+    print("Output file:    ", output_file)
     count = 0
     successful = 0
-    for f in inputFiles:
+    for f in input_files[3:4]:
         print("-------------------------")
         count += 1
         print("Current file: " + f)
         print("Post:  " + str(count))
-        p = Post(fileDir + "/" + f)
-        if p.parseFile() is False:
+        p = Post(file_dir + "/" + f)
+        if p.parse_file() is False:
             continue
         successful += 1
-        p.writeToFile(outputFile)
+        p.write_to_file(output_file)
 
     print("-------------------------")
     print("""Done!
 Parsed {}/{} posts from {}
-Now available in {}""".format(successful, count, fileDir, outputFile))
+Now available in {}""".format(successful, count, file_dir, output_file))
+
 
 if __name__ == '__main__':
-    main()
-
+    _main()
